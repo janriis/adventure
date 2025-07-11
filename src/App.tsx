b4b2bd2f1c278e26f76b51bd387bbcb73decd9f0
Jan Riis Sørensen <jan_riis@codamy.io>
2025-07-11 19:05:41 +0200

    changes to tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AdventureGame from './components/AdventureGame';
+import GameTutorial from './components/GameTutorial';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <AdventureGame debugMode={true} />
+      <GameTutorial />
     </div>
   );
 }
